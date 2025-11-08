de608b22cbd49fa27a50ccee3807ce701d367eab
Moita2025 <2380758219@qq.com>
2025-11-08 23:40:35 +1300

    fix: resolve `mathjax.js` `await` error issue

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,7 +1,7 @@
 window.MathJax = {
 	tex: {
-		inlineMath: [["$", "$"]],
-		displayMath: [["$$", "$$"]],
+		inlineMath: [["\\(", "\\)"]],
+		displayMath: [["\\[", "\\]"]],
 		processEscapes: true,
 		processEnvironments: true
 	},
@@ -11,9 +11,6 @@ window.MathJax = {
 	}
 };
 
-document$.subscribe( () => {
-	await MathJax.startup.output.clearCache();
-	MathJax.typesetClear();
-	MathJax.texReset();
+document$.subscribe(() => {
 	MathJax.typesetPromise();
 });
